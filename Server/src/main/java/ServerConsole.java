88f655f2e536b47ab699c5e12fbfd90d35518762
tendotheprogrammer <pmenoisy@gmail.com>
2025-02-04 10:32:48 +0200

    servercode

--- Server/src/main/java/ServerConsole.java
+++ Server/src/main/java/ServerConsole.java
@@ -1,5 +1,7 @@
 package Server.src.main.java;
 
+import Server.src.main.java.ServerCommands.ServerCommand;
+
 import java.util.Scanner;
 
 
@@ -8,6 +10,26 @@ public class ServerConsole implements Runnable {
     private static Scanner scanner = new Scanner(System.in);
 
     public void run() {
+        ServerCommand command;
+        boolean shouldContinue = true;
+        do {
+            String instruction = getInput();
+            try {
+                command = ServerCommand.create(instruction);
+                shouldContinue = command.execute();
+            } catch (IllegalArgumentException e) {
+                System.out.println("Sorry, I did not understand '" + instruction + "'.");
+                System.out.println(e.getMessage());
+            }
+        } while (shouldContinue);
+        System.exit(0);
+    }
 
+    private static String getInput() {
+        String input = "";
+        do{
+            input = scanner.nextLine();
+        }while (input.isBlank());
+        return input.strip().toLowerCase();
     }
 }
