df25adeee884a20e79567547708f33e557740fdb
tendotheprogrammer <pmenoisy@gmail.com>
2025-02-04 10:11:11 +0200

    servercode

---
+++ Server/src/main/java/ClientHandler.java
@@ -0,0 +1,31 @@
+package Server.src.main.java;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+
+    public static final int PORT = 6666;
+    public final BufferedReader in;
+    public final PrintStream out;
+    private static String clientMachine = "";
+    private final Socket socket;
+
+    public ClientHandler(Socket socket) throws IOException {
+        this.socket = socket;
+        clientMachine = this.socket.getInetAddress().getHostName();
+        System.out.printf("%s connected.\n",clientMachine);
+        out = new PrintStream(this.socket.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(
+                this.socket.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
---
+++ Server/src/main/java/ServerConsole.java
@@ -0,0 +1,13 @@
+package Server.src.main.java;
+
+import java.util.Scanner;
+
+
+public class ServerConsole implements Runnable {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public void run() {
+
+    }
+}
