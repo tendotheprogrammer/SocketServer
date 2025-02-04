4b4340f85c8073c0c9628888ee09335b513ac117
tendotheprogrammer <pmenoisy@gmail.com>
2025-02-04 14:09:07 +0200

    fixed client handlerm

--- Server/src/main/java/Response.java
+++ Server/src/main/java/Response.java
@@ -15,30 +15,5 @@ public class Response {
         return response.toJSONString();
     }
 
-    public static String getSuccessfulResponse(
-            HashMap<String, Object> data,
-            HashMap<String, Object> state)
-    {
-        JSONObject response = new JSONObject();
-        response.put("result", "OK");
-        response.put("data", data);
-        response.put("state", state);
-        return response.toJSONString();
-    }
 
-    public static HashMap<String, Object> createStateMap(
-            int[] position,
-            String direction,
-            int shields,
-            int shots,
-            String status)
-    {
-        HashMap<String, Object> state_map = new HashMap<>();
-        state_map.put("position", Arrays.asList(position[0],position[1]));
-        state_map.put("direction", direction);
-        state_map.put("shields", shields);
-        state_map.put("shots", shots);
-        state_map.put("status", status);
-        return state_map;
-    }
 }
